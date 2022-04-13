0ab0e48c4557a3d86509850fa2c3bb6a63a93bcf
tobybase <74760266+tobybase@users.noreply.github.com>
2022-04-13 16:20:13 +0800

    adding cart and meal item form

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import classes from './Cart.module.css';
+
+const Cart = (props) => {
+  const CartItems = (
+    <ul className={classes['cart-item']}>
+      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+  return (
+    <div>
+      {CartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.6</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
   {
@@ -29,13 +31,22 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
-      <ul>
-        <li>{mealsList}</li>
-      </ul>
+      <Card>
+        <ul>
+          <li>{mealsList}</li>
+        </ul>
+      </Card>
     </section>
   );
 };
